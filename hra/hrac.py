4334da70ecd9b0b437cd7c014a954080402a5ef9
marni <dinhtron@cvut.cz>
2026-07-16 10:44:06 +0200

    main, hrac

--- hra/hrac.py
+++ hra/hrac.py
@@ -1,12 +1,13 @@
 import pygame 
 
 class Hrac():
-    def __init__(self, jmeno ,sila=10):
+    def __init__(self, jmeno ,sila=10, x_pozice=0):
         self.jmeno = jmeno
         self.zivoty = 100
         self.sila = sila
+        self.x_pozice = x_pozice
 
     def vykresleni(self, obrazovka):
-        # Vykreslení hráče na obrazov
-        pygame.draw.rect(obrazovka, (0, 255, 0), (100, 100, 50, 50))  # Zelený čtverec jako hráč
-
+        # Vykreslení hráče na obrazov             
+        pygame.draw.rect(obrazovka, (0, 255, 0), (self.x_pozice, 500, 100, 300))  # Zelený čtverec jako hráč
+        #pozice (x,y,width,height)
--- hra/main.py
+++ hra/main.py
@@ -10,7 +10,8 @@ screen = pygame.display.set_mode((1400, 800))
 pygame.display.set_caption("Hello Pygame")
 
 # Game loop
-hra1 = Hrac("Hráč1")  # Call the vykresleni method to draw the player
+hra1 = Hrac("Hráč1",0, 0)  # Call the vykresleni method to draw the player
+hra2 = Hrac("Hráč2",0, 200)  # Create a second player with different attributes
 
   # Create an instance of the Hrac class
 running = True
@@ -24,6 +25,7 @@ while running:
 
     # 3. Vykreslení všech herních objektů
     hra1.vykresleni(screen)
+    hra2.vykresleni(screen)  # Draw the second player on the screen
 
     # 4. AKTUALIZACE OBRAZOVKY (to nejdůležitější, co ti chybělo!)
     pygame.display.flip()
